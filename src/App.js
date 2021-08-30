0e20425428bee9b9fb6677e738fa429deefdbff4
James Newton <blackslate@lexogram.com>
2021-08-30 17:15:23 +0300

    Simple react app to test setState with array
    * Create default app
    * Delete unneeded files
    * Edit src/index.js to remove references to deleted files
    * Edit App.js to create and display a list and a button to update it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [list, setList] = useState([1,2,3,4,5])
+
+  const updateList = () => {
+    const newList = list.map(item => getRandomNumber())
+     
+    setList(newList)
+  }
+
+  const getRandomNumber = () => {
+    return Math.ceil(Math.random() * 10)
+  }
+
+  const getList = () => {
+    const items = list.map((item, index) => <li key={index}>{item}</li>)
+    return <ul>{items}</ul>
+  }
+
+  const getButton = () => {
+    return (<
+      button
+      onClick={updateList}
+    >
+      Update List
+    </button>)
+  }
+
+  const ul = getList()
+  const button = getButton()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>{ul}{button}</div>
+  )
 }
 
 export default App;
