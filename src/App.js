a8840d48feb9a3f539dfa145a608b866f4ae2db3
James Newton <blackslate@lexogram.com>
2021-08-30 17:38:15 +0300

    Update to use to set<State> call; result = 1 render
    * Add otherList containing letters
    * Update both list and otherList in same updateLists() function
    * Modify gitList() function to accept any list as a parameter
    * Rename variables and button as appropriate
    * Add comments

--- src/App.js
+++ src/App.js
@@ -1,37 +1,55 @@
 import React, { useState } from 'react'
 
+let counter = 0
+
 function App() {
   const [list, setList] = useState([1,2,3,4,5])
+  const [otherList, setOtherList] = useState(["A", "B", "C"])
 
-  const updateList = () => {
-    const newList = list.map(item => getRandomNumber())
-     
-    setList(newList)
+  console.log("counter:", ++counter) // will jump by 2 each time
+  console.log("list:", list)
+  console.log("otherList:", otherList)
+
+  const updateLists = () => {
+    const numberList = list.map(item => getRandomNumber())   
+    const letterList =  otherList.map(item => getRandomLetter())
+
+    // Set two different state values for this component. React will
+    // call this function twice, but only render once. Both lists will
+    // have been updated before the component is rendered.
+    setList(numberList)
+    setOtherList(letterList)
   }
 
   const getRandomNumber = () => {
     return Math.ceil(Math.random() * 10)
   }
 
-  const getList = () => {
-    const items = list.map((item, index) => <li key={index}>{item}</li>)
+  const getRandomLetter = () => {
+    const charCode = 64 + Math.ceil(Math.random() * 26)
+    return String.fromCharCode(charCode)
+  }
+
+  const getList = (array) => {
+    const items = array.map((item, index) => <li key={index}>{item}</li>)
     return <ul>{items}</ul>
   }
 
   const getButton = () => {
     return (<
       button
-      onClick={updateList}
+      onClick={updateLists}
     >
-      Update List
+      Update Lists
     </button>)
   }
 
-  const ul = getList()
+  const numbers = getList(list)
+  const letters = getList(otherList)
   const button = getButton()
 
   return (
-    <div>{ul}{button}</div>
+    <div>{numbers}<hr />{letters}{button}</div>
   )
 }
 
